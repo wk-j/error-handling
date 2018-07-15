44d7a2cf37588289cd40f38d7bf402791e35b382
wk-j <somnuk.wk@gmail.com>
2018-07-16 02:05:29 +0700

    Initial commit

---
+++ src/ErrorHandling/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Cvdm.ErrorHandling
+
+type User = {
+    Name: string
+}
+
+let tryGetUser str = Some { Name = str }
+let isPwdValid pass user = user.Name = "wk" && pass = "wk"
+
+type LoginError = InvlidUser | InvalidPwd | Unauthroized of string
+
+[<EntryPoint>]
+let main argv =
+
+    result {
+        let! user = tryGetUser "wk" |> Result.requireSome InvlidUser
+        let! passOk = isPwdValid "wu" user |> Result.requireTrue InvalidPwd
+        return passOk
+    }
+    |> printfn "%A"
+
+    0 // return an integer exit code
